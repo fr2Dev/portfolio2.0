b526aeb126d02831536fc3caaa9b2b9e9f0814cd
fr2Dev <francois.degrincourt@gmail.com>
2019-02-17 17:55:38 +0100

    Finished js

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -4,6 +4,55 @@
 
   $(function () {
 
+    // Smooth nav links
+    var scrollLink = $('.scroll');
+  
+    // Smooth scrolling
+    scrollLink.click(function(e) {
+      e.preventDefault();
+      $('body,html').animate({
+        scrollTop: $(this.hash).offset().top
+      }, 1000 );
+    });
+    
+    // Active link switching
+    $(window).scroll(function() {
+      var scrollbarLocation = $(this).scrollTop();
+      
+      scrollLink.each(function() {
+        var sectionOffset = $(this.hash).offset().top - 20;
+        
+        if ( sectionOffset <= scrollbarLocation ) {
+          $(this).parent().addClass('active');
+          $(this).parent().siblings().removeClass('active');
+        }
+      })
+      
+    })
+
+    // Sticky header on scroll top
+
+    // grab an element
+    var myElement = document.querySelector('.header');
+    // construct an instance of Headroom, passing the element
+    var headroom  = new Headroom(myElement);
+    // initialise
+    headroom.init(); 
+
+     // uncheck nav on click
+    function uncheck() {
+      document.querySelector('.navbar__checkbox').checked = false;
+    }
+   
+    var navbar = document.querySelector('.navbar');
+
+    navbar.addEventListener('click', function (event) {
+
+      if (event.target.matches('.navbar__link')) {
+        uncheck();
+      }
+    }, false);
+
     // Contact form 
     var $contactForm = $('#contact-form');
 
@@ -73,6 +122,8 @@
       });
     });
     // END Contact form
+
+    
   });
 
 })(jQuery, window, document);
